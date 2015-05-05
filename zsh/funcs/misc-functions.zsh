55b0a5ec00524247a711f0533ff223d55b5bf94b
CJD14 <Diehl14C@gmail.com>
2015-05-05 12:58:01 -0400

    adding some extra's and changes

--- zsh/funcs/misc-functions.zsh
+++ zsh/funcs/misc-functions.zsh
@@ -14,3 +14,26 @@ function replace() {
   find . \( ! -regex '.*/\..*' \) -type f | xargs perl -pi -e "s/$1/$2/g"
 }
 
+#vim - thanks to earsplit for this!
+conf() {
+ case $1 in
+  tmux)    vim ~/.tmux.conf ;;
+  vim)     vim ~/.vimrc ;;
+  xd)      vim ~/.Xdefaults ;;
+  zsh)     vim ~/.zshrc && source ~/.zshrc ;;
+  alias)   aliasrc  && source ~/.dotfiles/zsh/aliases.zsh;;
+  i3)      vim ~/.config/i3/config ;;
+  pacman)  sudo vim /etc/pacman.conf ;;
+  mutt)    vim ~/.muttrc ;;
+  weechat) vim ~/.weechat/weechat.conf ;;
+  ls)      vim ~/.ls++.conf ;;
+  bash)    vim ~/.bashrc ;;
+  git)     vim ~/.gitconfig ;;
+  clip)    vim ~/.config/clipit/clipitrc ;;
+  termite) vim ~/.config/termite/config ;;
+  ranger)  vim ~/.config/ranger/rc.conf ;;
+  htop)    vim ~/.config/htop/htoprc ;;
+  dwb)     vim ~/.config/dwb/settings ;;
+  *)       echo "unknown conf: $1" ;;  
+ esac
+}
